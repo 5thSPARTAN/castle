e13eabe6c300494e5879b1d70f30a23dd81b33f8
5thSPARTAN <david.doberstein@gmail.com>
2025-06-02 23:22:59 -0700

    Reorganized files and now can call step()/reset() in python

--- src/PybindWrapper.cpp
+++ src/PybindWrapper.cpp
@@ -1,4 +1,6 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include "Observation.h"
 
 #include "GameEnv.h"
 
@@ -6,7 +8,11 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(game_env, m) {
     py::class_<GameEnv>(m, "GameEnv")
-    .def(py::init<int,int,int>())
-    .def("reset", &GameEnv::reset, "A function which resets the game")
-    .def("step", &GameEnv::step, py::arg("actions"));
+        .def(py::init<int,int,int>())
+        .def("reset", &GameEnv::reset, "A function which resets the game")
+        .def("step", &GameEnv::step, py::arg("actions"));
+    py::class_<Observation>(m, "Observation")
+        .def(py::init<>())
+        .def_readwrite("features", &Observation::features)
+        .def_readwrite("actionMask", &Observation::actionMask);
 }
