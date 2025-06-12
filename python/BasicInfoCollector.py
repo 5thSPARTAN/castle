553df4dc8e13f2790bff1d746c5951041f51229f
5thSPARTAN <david.doberstein@gmail.com>
2025-06-12 00:59:08 -0700

    Realized that the stolen cards need to be tracked too. also found out 1 million games takes about 30 minutes. seems like a good benchmark

--- python/BasicInfoCollector.py
+++ python/BasicInfoCollector.py
@@ -19,12 +19,13 @@ def is_power_of_10(n):
 
 
 def main():
-    NUMBER_OF_GAMES = 1000000000
+    NUMBER_OF_GAMES = 1000000
     NUMBER_OF_PLAYERS = 4
     STARTING_HEALTH = 5
     MAX_HEALTH = 7
 
     SPECIAL_TRACKING = 10
+    SPECIAL_TRACKING_STOLEN = 1
 
 
     env = GameEnv(NUMBER_OF_PLAYERS, STARTING_HEALTH, MAX_HEALTH)
@@ -41,9 +42,15 @@ def main():
     #   winning percentage
     total_wins = [0,0,0,0]
 
-    #   distribution of cards played by winners and compare it to distribution of cards played by losers (over the game)
+    # ROW 2, 4
+    #   distribution of cards played by winners and compare it to distribution of cards played by losers
     cards_played_by_losers_3d = [] # 3d array
     cards_played_by_winners_2d = [] # 2D array
+
+    # ROW 3, 5
+    #   distribution of cards stolen by winners and compare it to distribution of cards stolen by losers
+    cards_stolen_by_losers_3d = []
+    cards_stolen_by_winners_2d = []
 
 
 
@@ -56,6 +63,11 @@ def main():
         player2_cards = []
         player3_cards = []
 
+        player0_stolen = []
+        player1_stolen = []
+        player2_stolen = []
+        player3_stolen = []
+
         turn_count = 0 
 
         obs = env.reset()
@@ -67,6 +79,7 @@ def main():
                             select_random_action(obs[3].actionMask)
                             ]
             
+            # determine what cards won battle
             if all(action <= 24 for action in action_list) and all(action >= 14 for action in action_list):
                 winning_cards.append(determine_battle_winner(action_list))
             
@@ -74,6 +87,11 @@ def main():
             player1_cards.append(card_from_action_mask_index(action_list[1]))
             player2_cards.append(card_from_action_mask_index(action_list[2]))
             player3_cards.append(card_from_action_mask_index(action_list[3]))
+
+            if action_list[0] >= 1 and action_list[0] <= 13: player0_stolen.append(action_list[0])
+            if action_list[1] >= 1 and action_list[1] <= 13: player1_stolen.append(action_list[1])
+            if action_list[2] >= 1 and action_list[2] <= 13: player2_stolen.append(action_list[2])
+            if action_list[3] >= 1 and action_list[3] <= 13: player3_stolen.append(action_list[3])
 
             turn_count = turn_count + 1
 
@@ -86,19 +104,35 @@ def main():
             if game_end:
                 if rewards[0] == 1:
                     cards_played_by_winners_2d.append(player0_cards)
+                    cards_stolen_by_winners_2d.append(player0_stolen)
+
                     cards_played_by_losers_3d.append([player1_cards,player2_cards,player3_cards])
+                    cards_stolen_by_losers_3d.append([player1_stolen,player2_stolen,player3_stolen])
+
                     total_wins[0] += 1
                 elif rewards[1] == 1:
                     cards_played_by_winners_2d.append(player1_cards)
+                    cards_stolen_by_winners_2d.append(player1_stolen)
+
                     cards_played_by_losers_3d.append([player0_cards,player2_cards,player3_cards])
+                    cards_stolen_by_losers_3d.append([player0_stolen,player2_stolen,player3_stolen])
+
                     total_wins[1] += 1
                 elif rewards[2] == 1:
                     cards_played_by_winners_2d.append(player2_cards)
+                    cards_stolen_by_winners_2d.append(player2_stolen)
+
                     cards_played_by_losers_3d.append([player0_cards,player1_cards,player3_cards])
+                    cards_stolen_by_losers_3d.append([player0_stolen,player1_stolen,player3_stolen])
+
                     total_wins[2] += 1
                 elif rewards[3] == 1:
                     cards_played_by_winners_2d.append(player3_cards)
+                    cards_stolen_by_winners_2d.append(player3_stolen)
+
                     cards_played_by_losers_3d.append([player0_cards,player1_cards,player2_cards])
+                    cards_stolen_by_losers_3d.append([player0_stolen,player1_stolen,player2_stolen])
+
                     total_wins[3] += 1
                 # else:
                     # print("Something is happening")
@@ -112,13 +146,25 @@ def main():
         if is_power_of_10(game_number + 1):
             print(f"{game_number + 1} games completed")
             create_graph(winning_cards, 
-                    game_length, 
-                    total_wins, 
-                    cards_played_by_winners_2d, 
-                    cards_played_by_losers_3d,
-                    game_number + 1,
-                    SPECIAL_TRACKING)
-
+                game_length, 
+                total_wins, 
+                cards_played_by_winners_2d, 
+                cards_played_by_losers_3d,
+                cards_stolen_by_winners_2d,
+                cards_stolen_by_losers_3d,
+                game_number + 1,
+                SPECIAL_TRACKING,
+                SPECIAL_TRACKING_STOLEN)
+    # create_graph(winning_cards, 
+    #     game_length, 
+    #     total_wins, 
+    #     cards_played_by_winners_2d, 
+    #     cards_played_by_losers_3d,
+    #     cards_stolen_by_winners_2d,
+    #     cards_stolen_by_losers_3d,
+    #     game_number + 1,
+    #     SPECIAL_TRACKING,
+    #     SPECIAL_TRACKING_STOLEN)
 
 if __name__ == "__main__":
     main()
--- python/BasicInfoUtils.py
+++ python/BasicInfoUtils.py
@@ -13,6 +13,17 @@ def select_random_action( action_mask ):
         return random.choice(true_indicies)
     return -1
 
+def holistic_action_selector( action_mask ):
+    true_indicies = []
+    for i in range(len(action_mask)):
+        if action_mask[i] == True:
+            true_indicies.append(i)
+    
+    if len(true_indicies):
+        # work in progress
+        return 
+    return -1
+
 def card_from_action_mask_index( action_mask_index ):
     match action_mask_index:
         case 0|1|25|26|27|28:
@@ -21,6 +32,8 @@ def card_from_action_mask_index( action_mask_index ):
             return 2
         case _:
             return int(action_mask_index - 11)
+
+
 
 # precondition: action_list will lead to a battle
 # post condition: outputs the card value not the action mask index
@@ -38,10 +51,13 @@ def create_graph(winning_cards,
                   total_wins, 
                   cards_played_by_winners_2d, 
                   cards_played_by_losers_3d,
+                  cards_stolen_by_winners_2d,
+                  cards_stolen_by_losers_3d,
                   game_number,
-                  SPECIAL_TRACKING):
+                  SPECIAL_TRACKING,
+                  SPECIAL_TRACKING_STOLEN):
 
-    fig, axs = plt.subplots(3,3, figsize=(16,9))
+    fig, axs = plt.subplots(5,3, figsize=(16,12))
 
     # ROW 1 -----------------------------------------------------------------------------------------------------
     axs[0][0].hist(winning_cards, bins=[x for x in range(2,14)])
@@ -67,14 +83,14 @@ def create_graph(winning_cards,
     list_of_cards_played_by_winners = []
     for game in cards_played_by_winners_2d:
         for x in game:
-            if( x >= 2 and x <=14):
+            if( x >= 2 and x <=13):
                 list_of_cards_played_by_winners.append(x)
 
     list_of_cards_played_by_losers = []
     for game in cards_played_by_losers_3d:
         for player in game:
             for x in player:
-                if( x >= 2 and x <=14):
+                if( x >= 2 and x <=13):
                     list_of_cards_played_by_losers.append(x)
 
     clean_winners = np.array(list_of_cards_played_by_winners)
@@ -131,36 +147,27 @@ def create_graph(winning_cards,
     # ROW 3 -----------------------------------------------------------------------------------------------------
 
     # clean data
-    list_of_cards_played_by_winners = []
-    for game in cards_played_by_winners_2d:
-        count = 0
-        for x in reversed(game):
-            if count > SPECIAL_TRACKING:
-                break
-            if( x >= 2 and x <=14):
-                list_of_cards_played_by_winners.append(x)
-                count += 1
-            
+    list_of_cards_stolen_by_winners = []
+    for game in cards_stolen_by_winners_2d:
+        for x in game:
+            if( x >= 1 and x <=13):
+                list_of_cards_stolen_by_winners.append(x)
 
-    list_of_cards_played_by_losers = []
-    for game in cards_played_by_losers_3d:
+    list_of_cards_stolen_by_losers = []
+    for game in cards_stolen_by_losers_3d:
         for player in game:
-            count = 0
             for x in player:
-                if count > SPECIAL_TRACKING:
-                    break
-                if( x >= 2 and x <=14):
-                    list_of_cards_played_by_losers.append(x)
-                    count += 1
+                if( x >= 1 and x <=13):
+                    list_of_cards_stolen_by_losers.append(x)
 
-    clean_winners = np.array(list_of_cards_played_by_winners)
-    clean_losers = np.array(list_of_cards_played_by_losers)
+    clean_winners = np.array(list_of_cards_stolen_by_winners)
+    clean_losers = np.array(list_of_cards_stolen_by_losers)
 
     winner_percents = []
     loser_percents = []
 
-    counts, bins, patches = axs[2][0].hist(clean_winners, align="left", bins=[x for x in range(2,15)], density=True)
-    axs[2][0].set_title(f"Card's Played by Winners\nin Last {SPECIAL_TRACKING} Turns")
+    counts, bins, patches = axs[2][0].hist(clean_winners, align="left", bins=[x for x in range(1,15)], density=True)
+    axs[2][0].set_title("Card's Stolen by Winners")
 
     norm = plt.Normalize(min(counts), max(counts))
     cmap = plt.cm.coolwarm
@@ -178,14 +185,90 @@ def create_graph(winning_cards,
 
         winner_percents.append(count)
 
-    counts, bins, patches = axs[2][1].hist(clean_losers, align="left", bins=[x for x in range(2,15)], density=True)
-    axs[2][1].set_title(f"Card's Played by Losers\nin Last {SPECIAL_TRACKING} Turns")
+    counts, bins, patches = axs[2][1].hist(clean_losers, align="left", bins=[x for x in range(1,15)], density=True)
+    axs[2][1].set_title("Card's Stolen by Losers")
 
     norm = plt.Normalize(min(counts), max(counts))
     cmap = plt.cm.coolwarm
 
     for count, patch in zip(counts,patches):
         axs[2][1].text(
+            patch.get_x() + patch.get_width()/2,
+            count,
+            f"{(count*100):.2f}%",
+            fontsize=7,
+            ha="center",
+            va="bottom"
+            )
+        patch.set_facecolor(cmap(norm(count)))
+
+        loser_percents.append(count)
+
+
+    x_values = [x for x in range(1,14)]
+    clean_difference = np.array(winner_percents) - np.array(loser_percents)
+
+    axs[2][2].bar(x_values, clean_difference)
+    axs[2][2].set_title("Percent Difference Between\nWinning and Losing")
+
+    # ROW 4 -----------------------------------------------------------------------------------------------------
+
+    # clean data
+    list_of_cards_played_by_winners = []
+    for game in cards_played_by_winners_2d:
+        count = 0
+        for x in reversed(game):
+            if count > SPECIAL_TRACKING:
+                break
+            if( x >= 2 and x <=13):
+                list_of_cards_played_by_winners.append(x)
+                count += 1
+            
+
+    list_of_cards_played_by_losers = []
+    for game in cards_played_by_losers_3d:
+        for player in game:
+            count = 0
+            for x in player:
+                if count > SPECIAL_TRACKING:
+                    break
+                if( x >= 2 and x <=13):
+                    list_of_cards_played_by_losers.append(x)
+                    count += 1
+
+    clean_winners = np.array(list_of_cards_played_by_winners)
+    clean_losers = np.array(list_of_cards_played_by_losers)
+
+    winner_percents = []
+    loser_percents = []
+
+    counts, bins, patches = axs[3][0].hist(clean_winners, align="left", bins=[x for x in range(2,15)], density=True)
+    axs[3][0].set_title(f"Card's Played by Winners\nin Last {SPECIAL_TRACKING} Turns")
+
+    norm = plt.Normalize(min(counts), max(counts))
+    cmap = plt.cm.coolwarm
+
+    for count, patch in zip(counts,patches):
+        axs[3][0].text(
+            patch.get_x() + patch.get_width()/2,
+            count,
+            f"{(count*100):.2f}%",
+            fontsize=7,
+            ha="center",
+            va="bottom"
+            )
+        patch.set_facecolor(cmap(norm(count)))
+
+        winner_percents.append(count)
+
+    counts, bins, patches = axs[3][1].hist(clean_losers, align="left", bins=[x for x in range(2,15)], density=True)
+    axs[3][1].set_title(f"Card's Played by Losers\nin Last {SPECIAL_TRACKING} Turns")
+
+    norm = plt.Normalize(min(counts), max(counts))
+    cmap = plt.cm.coolwarm
+
+    for count, patch in zip(counts,patches):
+        axs[3][1].text(
             patch.get_x() + patch.get_width()/2,
             count,
             f"{(count*100):.2f}%",
@@ -201,8 +284,88 @@ def create_graph(winning_cards,
     x_values = [x for x in range(2,14)]
     clean_difference = np.array(winner_percents) - np.array(loser_percents)
 
-    axs[2][2].bar(x_values, clean_difference)
-    axs[2][2].set_title(f"Percent Difference Between\nWinning Cards and Losing Cards\nOver Last {SPECIAL_TRACKING} Turns")
+    axs[3][2].bar(x_values, clean_difference)
+    axs[3][2].set_title(f"Percent Difference Between\nWinning Cards and Losing Cards\nOver Last {SPECIAL_TRACKING} Turns")
+
+    # ROW 5 -----------------------------------------------------------------------------------------------------
+
+    # clean data
+    list_of_cards_stolen_by_winners = []
+    for game in cards_stolen_by_winners_2d:
+        count = 0
+        for x in reversed(game):
+            if count > SPECIAL_TRACKING_STOLEN:
+                break
+            if( x >= 1 and x <=13):
+                list_of_cards_stolen_by_winners.append(x)
+                count += 1
+            
+
+    list_of_cards_stolen_by_losers = []
+    for game in cards_stolen_by_losers_3d:
+        for player in game:
+            count = 0
+            for x in player:
+                if count > SPECIAL_TRACKING_STOLEN:
+                    break
+                if( x >= 1 and x <=13):
+                    list_of_cards_stolen_by_losers.append(x)
+                    count += 1
+
+    clean_winners = np.array(list_of_cards_stolen_by_winners)
+    clean_losers = np.array(list_of_cards_stolen_by_losers)
+
+    winner_percents = []
+    loser_percents = []
+
+    counts, bins, patches = axs[4][0].hist(clean_winners, align="left", bins=[x for x in range(1,15)], density=True)
+    axs[4][0].set_title(f"Last {SPECIAL_TRACKING_STOLEN} Cards Stolen By Winners")
+
+    norm = plt.Normalize(min(counts), max(counts))
+    cmap = plt.cm.coolwarm
+
+    for count, patch in zip(counts,patches):
+        axs[4][0].text(
+            patch.get_x() + patch.get_width()/2,
+            count,
+            f"{(count*100):.2f}%",
+            fontsize=7,
+            ha="center",
+            va="bottom"
+            )
+        patch.set_facecolor(cmap(norm(count)))
+
+        winner_percents.append(count)
+
+    counts, bins, patches = axs[4][1].hist(clean_losers, align="left", bins=[x for x in range(1,15)], density=True)
+    axs[4][1].set_title(f"Last {SPECIAL_TRACKING_STOLEN} Cards Stolen By Losers")
+
+    norm = plt.Normalize(min(counts), max(counts))
+    cmap = plt.cm.coolwarm
+
+    for count, patch in zip(counts,patches):
+        axs[4][1].text(
+            patch.get_x() + patch.get_width()/2,
+            count,
+            f"{(count*100):.2f}%",
+            fontsize=8,
+            ha="center",
+            va="bottom"
+            )
+        patch.set_facecolor(cmap(norm(count)))
+
+        loser_percents.append(count)
+
+
+    x_values = [x for x in range(1,14)]
+    clean_difference = np.array(winner_percents) - np.array(loser_percents)
+
+    axs[4][2].bar(x_values, clean_difference)
+    axs[4][2].set_title(f"Percent Difference Between Winners and Losers")
+
+
+    # -----------------------------------------------------------------------------------------------------------
 
     plt.tight_layout()
-    fig.savefig(f"../graphs/Basic_Info:_{game_number}_TESTS_{SPECIAL_TRACKING}_SPECIAL.png", dpi=300)
+    fig.savefig(f"../graphs/Basic_Info:_RANDOM_{game_number}_TESTS.png", dpi=300)
+    #plt.show()
